721199f6cd486b4380491e0da314f2e4ba5567ae
Bijan Kundu <bijankundu025@gmail.com>
2020-04-23 09:37:18 +0530

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import SpeakerNotesIcon from "@material-ui/icons/SpeakerNotes";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        {" "}
+        <SpeakerNotesIcon />
+        Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
